6fa95194a6aafc681a2b70d070c7aab8c6a92f46
Morganero <morganero1@gmail.com>
2022-10-07 10:09:11 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+const dishes = [
+    "Dinner plate", "Water glass", "Salad bowl", "Dinner plate", "Wine glass", "Spoon", "Spoon", "Fork", "Salad bowl", "Whiskey glass", "Fork", "Spoon", "Fork"
+]
+/* Declare three variables to store the string value of a glass in the array. use the correct index to get the right value.
+*/
+dirtyGlassOne = dishes[1];
+dirtyGlassOTwo = dishes[4];
+dirtyGlassThree = dishes[9];
+
+
+console.log("I am cleaning the following glasses:")
+
+//Put each variable in one of the parenthesis below
+console.log(dirtyGlassOne)
+console.log(dirtyGlassOTwo)
+console.log(dirtyGlassThree)
